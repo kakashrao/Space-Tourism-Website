2b7e44f5da28c3207997e44b3dc7803455946dc1
Akash <kakashrao.2021@gmail.com>
2021-12-30 23:08:07 +0530

    HomeComponent Completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrewComponent } from './crew/crew.component';
+import { DestinationComponent } from './destination/destination.component';
 import { HomeComponent } from './home/home.component';
+import { TechnologyComponent } from './technology/technology.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent},
+  { path: 'destination', component: DestinationComponent},
+  { path: 'crew', component: CrewComponent},
+  { path: 'tech', component: TechnologyComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { DestinationComponent } from './destination/destination.component';
 import { CrewComponent } from './crew/crew.component';
 import { TechnologyComponent } from './technology/technology.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TechnologyComponent } from './technology/technology.component';
     HomeComponent,
     DestinationComponent,
     CrewComponent,
-    TechnologyComponent
+    TechnologyComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
