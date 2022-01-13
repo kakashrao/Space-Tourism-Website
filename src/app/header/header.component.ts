d3c902eea3654c488962f56e5efc92de30ff9569
Akash <kakashrao.2021@gmail.com>
2022-01-13 21:47:19 +0530

    checks

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,22 +4,21 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  tabs: string[] = ['home', 'destination', 'crew', 'technology'] 
+  tabs: string[] = ['home', 'destination', 'crew', 'technology'];
   index: number = 0;
-  srcHeight : number = 0;
-  srcWidth : number = 0;
+  srcHeight: number = 0;
+  srcWidth: number = 0;
   openSideBar: boolean = false;
   activeTab: string = 'home';
 
-  constructor(private router: Router) { 
+  constructor(private router: Router) {
     this.getScreenSize();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   @HostListener('window:resize')
   getScreenSize() {
@@ -28,10 +27,10 @@ export class HeaderComponent implements OnInit {
     // console.log(this.srcHeight, this.srcWidth);
   }
 
-onChangeTabs(index: number) {
-  this.index = index;
-  this.router.navigate([this.tabs[index]]);
-}
+  onChangeTabs(index: number) {
+    this.index = index;
+    this.router.navigate([this.tabs[index]]);
+  }
 
   toggleSidebar() {
     this.openSideBar = true;
